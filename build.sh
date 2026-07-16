a98c33578b1d964599944f1393034c3e9770cb5f
Eric N <120667897+ninja888-8@users.noreply.github.com>
2026-07-15 22:56:56 -0400

    fixes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 echo "Downloading Stockfish..."
 wget -q https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64.tar
 tar -xf stockfish-ubuntu-x86-64.tar
-mv stockfish/stockfish-ubuntu-x86-64 ./stockfish
+find . -name "stockfish*" -type f -not -name "*.tar" -exec mv {} ./stockfish \;
 rm -rf stockfish-ubuntu-x86-64.tar stockfish/
 
 chmod +x ./stockfish
