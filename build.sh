c4c394db100fd1e505cf91976c5631096ba947cc
Eric N <120667897+ninja888-8@users.noreply.github.com>
2026-07-15 22:40:45 -0400

    add bash file

---
+++ build.sh
@@ -0,0 +1,12 @@
+set -e
+
+pip install -r requirements.txt
+
+echo "Downloading Stockfish..."
+wget -q https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64.tar
+tar -xf stockfish-ubuntu-x86-64.tar
+mv stockfish/stockfish-ubuntu-x86-64 ./stockfish
+rm -rf stockfish-ubuntu-x86-64.tar stockfish/
+
+chmod +x ./stockfish
+echo "Process complete"
